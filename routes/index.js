45653570f451dd93c093a6627b64d9c44c19fca6
ubuntu <jumbo4213@163.com>
2018-11-02 15:32:11 +0800

    增加数据库连接

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router=require("koa-router")();
 router.get("/",async (ctx)=>{
     ctx.body="首页1";
+    ctx.session.user="jumbo";
 });
 router.use("/login",require("./login"));
 module.exports=router;
--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 const router=require("koa-router")();
 router.get("/",async (ctx)=>{
+    console.log(ctx.session.user);
     ctx.body="登陆页";
 });
 module.exports=router.routes();
